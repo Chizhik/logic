9c7bd8e0502477066ce9469871de5332a22c35cd
alisher <t.b.alisher@gmail.com>
2017-04-08 20:48:50 +0900

    99x99 works

--- nonogram.py
+++ nonogram.py
@@ -3,16 +3,13 @@ import subprocess
 f = open("nonogram.in", "r")
 n = int(f.readline())
 m = int(f.readline())
-print(n, m)
 conjs = []
 next_free_idx = n*m + 1
 # n - number of rows
 for i in range(n):
     rule = list(map(int, (f.readline().split())))
-    print(rule)
     # number of additional variables for each number in rule
     r_vars = n - sum(rule) - len(rule) + 2
-    print(r_vars)
     # Part 1
     for j in range(len(rule)):
         # for each number in rule at least one of its additional variables is True
@@ -40,7 +37,6 @@ for i in range(n):
                 if e+k >= j+1 >= s+k:
                     corr.append(str(next_free_idx+idx*r_vars+k))
             s = e+2
-        print(corr)
         # var[i*m+j] is True if and only if disjunction of corresponding additional variables is True
         conjs.append('-'+str(i*m+j+1)+' '+' '.join(corr))
         for x in corr:
@@ -49,10 +45,8 @@ for i in range(n):
 # m - number of columns
 for i in range(m):
     rule = list(map(int, (f.readline().split())))
-    print(rule)
     # number of additional variables for each number in rule
     r_vars = m - sum(rule) - len(rule) + 2
-    print(r_vars)
     # Part 1
     for j in range(len(rule)):
         # for each number in rule at least one of its additional variables is True
@@ -80,18 +74,15 @@ for i in range(m):
                 if e+k >= j+1 >= s+k:
                     corr.append(str(next_free_idx+idx*r_vars+k))
             s = e+2
-        print(corr)
         # var[j*m+i] is True if and only if disjunction of corresponding additional variables is True
         conjs.append('-'+str(j*m+i+1)+' '+' '.join(corr))
         for x in corr:
             conjs.append(str(j*m+i+1)+' -'+x)
     next_free_idx += r_vars*len(rule)
-print(conjs)
 f.close()
 # prepare input for minisat
 mf = open('minisat.in', 'w')
 total_vars = next_free_idx - 1
-print("total vars: ", total_vars)
 mf.write('p cnf '+str(total_vars)+' '+str(len(conjs)))
 for c in conjs:
     mf.write('\n')
