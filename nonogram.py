7d294a33092f4143cc6eff10122b721f0abc673e
alisher <t.b.alisher@gmail.com>
2017-04-08 15:26:19 +0900

    part 2

--- nonogram.py
+++ nonogram.py
@@ -10,6 +10,7 @@ for i in range(n):
     # number of additional variables for each number in rule
     r_vars = n - sum(rule) - len(rule) + 2
     print(r_vars)
+    # Part 1
     for j in range(len(rule)):
         # for each number in rule at least one of its additional variables is True
         conjs.append(' '.join([str(k) for k in range(next_free_idx+j*r_vars, next_free_idx+(j+1)*r_vars)]))
@@ -17,5 +18,17 @@ for i in range(n):
         for k in range(next_free_idx+j*r_vars, next_free_idx+(j+1)*r_vars):
             for l in range(k+1, next_free_idx+(j+1)*r_vars):
                 conjs.append('-'+str(k)+' -'+str(l))
+    # Part 2
+    for j in range(len(rule)-1):
+        offset = next_free_idx+j*r_vars
+        # if var[offset+k] is True then var[offset+r_vars+k] or var[offset+r_vars+k+1] or var[offset+r_vars+k+2] or ...
+        # we can ignore var[offset] because var[offset+r_vars] or var[offset+r_vars+1] or ... is already in part 1
+        for k in range(1, r_vars):
+            conjs.append('-'+str(offset+k)+' '+' '.join([str(offset+r_vars+l) for l in range(k, r_vars)]))
+        # there is no need to do same things backwards because of part 1 (exactly one is true)
+    # Part 3
+
+
+
     next_free_idx += r_vars*len(rule)
 print(conjs)
